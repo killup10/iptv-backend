4cf5a36701c3da85751686011a12d8ae70c27455
killup10 <juanjose.gold10@gmail.com>
2025-04-30 22:24:43 -0500

    Fix ruta POST M3U con authenticateToken

--- routes/m3u.routes.js
+++ routes/m3u.routes.js
@@ -1,12 +1,12 @@
 // routes/m3u.routes.js
 import express from "express";
 import { uploadM3U } from "../controllers/m3u.controller.js";
-import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { authenticateToken } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 // Ruta para subir M3U (POST)
-router.post("/upload", authMiddleware, uploadM3U);
+router.post("/upload", authenticateToken, uploadM3U);
 
 // Ruta de prueba (opcional)
 router.get("/", (req, res) => {
